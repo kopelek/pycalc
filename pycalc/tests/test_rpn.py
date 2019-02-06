36e88e2cb38d6deb0a6795ed8edd7b02d0ac83e9
kopelek <47242789+kopelek@users.noreply.github.com>
2019-02-06 10:58:30 +0100

    Add tests for cases from home task description
    Math module methods and implicit multiplication.

--- pycalc/tests/test_rpn.py
+++ pycalc/tests/test_rpn.py
@@ -31,6 +31,17 @@ class TestRPN(unittest.TestCase):
     def test_convert_calculate_round(self):
         self.assertEqual(2.1, self._rpn.calculate(self._rpn.convert('round(2.123, 1)')))
 
+    def test_math_module_sin_pi(self):
+        self.assertEqual(1.0, self._rpn.calculate(self._rpn.convert('sin(pi/2)')))
+
+    def test_implicit_multiplication_case_1(self):
+        self.assertEqual(14, self._rpn.calculate(self._rpn.convert('2(5+2)')))
+
+    def test_implicit_multiplication_case_2(self):
+        self.assertEqual(21, self._rpn.calculate(self._rpn.convert('(1 + 2)(3 + 4)')))
+
+    def test_implicit_multiplication_case_3(self):
+        self.assertEqual(5, self._rpn.calculate(self._rpn.convert('log10(10)5')))        
 
 if __name__ == "__main__":
     unittest.main()
