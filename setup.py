9955b3bfa48896da135e379a40d0ca3946e8ca24
kopelek <47242789+kopelek@users.noreply.github.com>
2019-02-26 21:29:17 +0100

    version 0.0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
   
 setup(
     name='pycalc-kkopel',
-    version='0.0.1',
+    version='0.0.2',
     author='Krzysztof Kopel',
     author_email='krzysztof_kopel@epam.com',
     description='Pure Python calculator.',
